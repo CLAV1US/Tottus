7b6dfd07ee3e03fdcbbd1758668679ef832bc951
Francisco David Gajardo <franciscodavidgajardo@MacBook-Pro-de-equipamiento.local>
2019-08-12 17:33:06 -0400

    Project Update

--- features/google.feature
+++ features/google.feature
@@ -2,14 +2,15 @@ Feature: Buscar sucursal Tottus en Google
 
   Necesito buscar una sucrusal de Tottus especifica en Google
 
-  Scenario: Buscando sucursal Tottus en Google!!!
+  Scenario: Buscando sucursal Tottus en Google
     Given Abrir pagina busqueda Google
     When Realizando busqueda de "tottus sucursales" en Google
     Then Presionar "enter" en Google
-    When Validar resultados seleccionando sitio web de "Tottus Nataniel"
-    Then Presionar en link Recetas y mas
-    Then Presionar en nivel alto
-    Then Seleccionar Corona de rollitos de canela
+    When Validar resultados seleccionando sitio web de "Tottus express Cerro Blanco"
+    Then Acceder a Tottus
+    #Then Presionar en link Recetas y mas
+    #Then Presionar en nivel alto
+    #Then Seleccionar Corona de rollitos de canela
 
 
   Scenario: Directo
--- features/step_definitions/google.js
+++ features/step_definitions/google.js
@@ -3,6 +3,7 @@ const Selector = require('testcafe').Selector;
 
 Given('Abrir pagina busqueda Google', async function() {
     await testController.navigateTo('https://www.google.cl');
+    await this.addScreenshotToReport();
 });
 
 When('Realizando busqueda de {string} en Google', async function(text) {
@@ -17,28 +18,41 @@ Then('Presionar {string} en Google', async function(text) {
 });
 
 When('Validar resultados seleccionando sitio web de {string}', async function(text) {
-    var firstLink = Selector('#rso').find('div.BSaJxc').withText('').with({boundTestRun: testController});
+    var firstLink = Selector('#rso').find("span").withText(text).with({boundTestRun: testController});
     await this.addScreenshotToReport();
     await testController.click(firstLink);
 });
 
+Then('Acceder a Tottus', async function() {
+    await (20000);
+    var acceso = Selector('.CL9Uqc.ab_button').with({boundTestRun: testController});
+    await this.addScreenshotToReport();
+    await testController.click(acceso);
+});
+
 Then('Presionar en link Recetas y mas', async function() {
 	await (20000);
-	var firstLink = Selector('#new_tottus_nav_cliente_recetas').find('img').with({boundTestRun: testController});
+	var receta = Selector('#new_tottus_nav_cliente_recetas').find("img").withText('receta').with({boundTestRun: testController});
     await this.addScreenshotToReport();
-    await testController.click('receta');
+    await testController.click(receta);
 });
 
 Then('Presionar en nivel alto', async function() {
-	await this.addScreenshotToReport();
-	await testController.click('[onclick*="clickfilterMo(3)"]');
+    var nivel = Selector('#myDIV > button:nth-child(4)').with({boundTestRun: testController});
+	await testController.click(nivel);
+    await this.addScreenshotToReport();
 });
 
 Given('Abrir pagina busqueda Tottus', async function() {
+    this.addScreenshotToReport();
     await testController.navigateTo('http://www.tottusrecetas.cl/');
+    this.addScreenshotToReport();
 });
 
 Then('Seleccionar Corona de rollitos de canela', async function() {
-    await this.addScreenshotToReport();
-    await testController.click(Selector('#divRecipes>section>div>div:nth-child(4)>div:nth-child(2)>div>div.card-desc').find('h3').withText('Corona de rollitos de canela'));
+    this.addScreenshotToReport();
+    var canela = Selector('#divRecipes > section > div > div:nth-child(4) > div:nth-child(2) > div > div.card-desc > h3').with({boundTestRun: testController});
+    await testController.click(canela);
+    this.addScreenshotToReport();
+
 });
